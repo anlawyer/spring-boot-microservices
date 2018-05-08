11c6f14596c5d82b9180bcb3a7ae21b16652061d
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-08 16:36:17 -0700

    added modified files

---
+++ songs-api/src/main/resources/db/migration/V1__Create_song_table.sql
@@ -0,0 +1,5 @@
+create table USERS (
+  ID serial,
+  TITLE varchar(100) NOT NULL,
+  LENGTH int
+);
---
+++ songs-api/src/main/resources/db/migration/afterMigration.sql
@@ -0,0 +1,6 @@
+INSERT INTO SONGS
+    (TITLE, LENGTH)
+VALUES
+    ('Instant Need', 4.30),
+    ('Full Sail', 2.34),
+    ('Beachside', 3.03);
